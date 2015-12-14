372653a0a86e3f0be17a2270c2774f1e24b8e163
Jef Amirault <admiraljef@gmail.com>
2015-12-14 15:20:20 -0500

    updated nav link highlighting. Mockups removed, new-review and change-password now have nav link highlighting

--- public/javascripts/nav.js
+++ public/javascripts/nav.js
@@ -5,13 +5,11 @@ $(document).ready(function(){
         selected = 'home_link';
     else if (path == '/about')
         selected = 'about_link';
-    else if (path.substring(0,8) == '/mockups')
-        selected = 'mockups_link';
-    else if (path.substring(0,15) == '/dining_commons')
+    else if (path.substring(0,15) == '/dining_commons' || path == '/reviews/new')
         selected = 'dining_commons_link';
     else if (path == '/users/login' || path == '/users/signup')
         selected = 'login_link';
-    else if (path == '/users/profile')
+    else if (path == '/users/profile' || path == '/users/change-password')
         selected = 'profile_link';
     else if (path == '/users')
         selected = 'users_link';
